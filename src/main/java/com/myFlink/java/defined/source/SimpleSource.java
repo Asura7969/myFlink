c5a2d0044cb090754c976942c9c91b4eb7a5fb9e
gongwenzhou <1402357969@qq.com>
2019-01-23 18:44:15 +0800

    自定义source添加checkpoint

--- src/main/java/com/myFlink/java/defined/source/SimpleSource.java
+++ src/main/java/com/myFlink/java/defined/source/SimpleSource.java
@@ -1,11 +1,17 @@
 package com.myFlink.java.defined.source;
 
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.runtime.state.FunctionInitializationContext;
+import org.apache.flink.runtime.state.FunctionSnapshotContext;
+import org.apache.flink.streaming.api.checkpoint.CheckpointedFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
-public class SimpleSource implements SourceFunction<Long>{
+public class SimpleSource implements SourceFunction<Long>, CheckpointedFunction {
 
     private long count = 0L;
     private volatile boolean isRunning;
+    private transient ListState<Long> checkpointedCount;
 
     @Override
     public void run(SourceContext<Long> sct) throws Exception {
@@ -20,5 +26,24 @@ public class SimpleSource implements SourceFunction<Long>{
     @Override
     public void cancel() {
         isRunning = false;
+    }
+
+    @Override
+    public void snapshotState(FunctionSnapshotContext context) throws Exception {
+        this.checkpointedCount.clear();
+        this.checkpointedCount.add(count);
+    }
+
+    @Override
+    public void initializeState(FunctionInitializationContext context) throws Exception {
+        this.checkpointedCount = context
+                .getOperatorStateStore()
+                .getListState(new ListStateDescriptor<>("count",Long.class));
+
+        if (context.isRestored()) {
+            for (Long count : this.checkpointedCount.get()) {
+                this.count = count;
+            }
+        }
     }
 }
